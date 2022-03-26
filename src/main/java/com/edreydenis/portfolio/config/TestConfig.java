ad23f3dd448706c1e41c4e7544273e6463b04064
Denis Edrey <denis.alves.hu@gmail.com>
2022-03-26 19:20:58 -0300

    added payment

--- src/main/java/com/edreydenis/portfolio/config/TestConfig.java
+++ src/main/java/com/edreydenis/portfolio/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.edreydenis.portfolio.entities.Category;
 import com.edreydenis.portfolio.entities.Order;
 import com.edreydenis.portfolio.entities.OrderItem;
+import com.edreydenis.portfolio.entities.Payment;
 import com.edreydenis.portfolio.entities.Product;
 import com.edreydenis.portfolio.entities.User;
 import com.edreydenis.portfolio.entities.enums.OrderStatus;
@@ -80,6 +81,12 @@ public class TestConfig implements CommandLineRunner{
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 		
 	}
 	
