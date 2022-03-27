4284de7a9f94a1e6f62ea82c03f3aa70b90eb3c9
Denis Edrey <denis.alves.hu@gmail.com>
2022-03-27 17:08:38 -0300

    added exception handlers

--- src/main/java/com/edreydenis/portfolio/services/UserService.java
+++ src/main/java/com/edreydenis/portfolio/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.edreydenis.portfolio.entities.User;
 import com.edreydenis.portfolio.repositories.UserRepository;
+import com.edreydenis.portfolio.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -21,7 +22,7 @@ public class UserService {
 	
 	public User findById(Long id) {
 		Optional<User> obj = repository.findById(id);
-		return obj.get();
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
 	
 	public User insert (User obj) {
